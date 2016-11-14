5c8f58b69176a074de0087fd1092a013b227b379
fchgds <fernandochavez@zoftco.com>
2016-11-13 20:26:19 -0400

    Init

---
+++ inc/config.php
@@ -0,0 +1,12 @@
+<?php
+	define('DB_HOST', 'localhost');
+	define('DB_USER', 'root');
+	define('DB_PASSWORD', '');
+	define('DB_NAME', 'clein');
+	define('HOME_PAGE', 'admin.php');
+	define('CONTACT_MAIL', 'info@cleinparaguay.com');
+	define('MAIL_SERVER_HOST', '');
+	define('MAIL_SERVER_PASS', '');
+	define('MAIL_SERVER_USER', '');
+	define('MAIL_SERVER_PORT', '25');
+?>
